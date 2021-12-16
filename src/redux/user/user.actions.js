06065e60474605e1ec0746874da7c16fe9857003
Franklin Lainez <franklin.lainez2805@gmail.com>
2021-12-16 16:51:28 -0600

    fixing importing error

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,6 @@
-import { SET_CURRENT_USER } from "./users.types";
+import { userActionTypes } from "./users.types";
 
 export const setCurrentUser = (user) => ({
-  type: SET_CURRENT_USER,
+  type: userActionTypes.SET_CURRENT_USER,
   payload: user,
 });
